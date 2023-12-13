b1b134c3e8a289630426b63076671a7dbba1fae5
Crunchies3 <113993166+Crunchies3@users.noreply.github.com>
2023-12-13 23:02:37 +0800

    remove loginPage import in auth_page.dart

--- lib/page/auth_page.dart
+++ lib/page/auth_page.dart
@@ -1,6 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:yomu_reader_1/page/login_page.dart';
 
 import 'home_page.dart';
 import 'login_or_register_page.dart';
