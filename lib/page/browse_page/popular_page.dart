a90f6f7a8e0532b4570e7fcf320544ade873374d
Crunchies3 <113993166+Crunchies3@users.noreply.github.com>
2023-12-14 23:51:37 +0800

    added sample picture for grid view. can now properly use grid view

--- lib/page/browse_page/popular_page.dart
+++ lib/page/browse_page/popular_page.dart
@@ -4,6 +4,36 @@ class PopularPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Padding(
+      padding: const EdgeInsets.all(5.0),
+      child: GridView.builder(
+        shrinkWrap: false,
+          itemCount: 10,
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2, mainAxisSpacing: 5, childAspectRatio: 1/1.75,),
+          itemBuilder: (context, index) {
+            return Padding(
+              padding: EdgeInsets.all(5),
+              child: Column(
+                children: [
+                  Container(
+                    color: Colors.red,
+                    child: Image(
+                      fit: BoxFit.contain,
+                      image: AssetImage("asset/image/one_piece.jpg"),
+                    ),
+                  ),
+                  SizedBox(height: 10,),
+                  Row(
+                    children: [
+                      Text("One piece")
+                    ],
+                  ),
+                  SizedBox(height: 10,),
+                ],
+              ),
+            );
+          }
+      ),
+    );
   }
 }
