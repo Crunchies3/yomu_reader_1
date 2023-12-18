37da489d6fa7e98a926e6775392497a270b5d82f
Crunchies3 <113993166+Crunchies3@users.noreply.github.com>
2023-12-18 21:42:46 +0800

    used cached to save loading time, also added circular loading

--- lib/page/screen/manga_content.dart
+++ lib/page/screen/manga_content.dart
@@ -1,7 +1,9 @@
 import 'dart:convert';
 
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+
 class MangaContent extends StatefulWidget {
   final String mangaTitle;
   final String chapterId;
@@ -18,49 +20,59 @@ class MangaContent extends StatefulWidget {
 }
 
 class _MangaContentState extends State<MangaContent> {
-
   List<dynamic> dataSaver = [];
   List<dynamic> pages = [""];
+  bool isLoading = true;
 
   var hash = "";
   var baseUrl = "";
 
-
-
   void onLoad() async {
-
-    try{
+    try {
       final id = widget.chapterId;
       const String url = 'https://api.mangadex.org';
-      final response = await http.get(
-        Uri.parse('$url/at-home/server/$id')
-      );
+      final response = await http.get(Uri.parse('$url/at-home/server/$id'));
       final responseData = jsonDecode(response.body);
       setState(() {
         dataSaver = responseData["chapter"]["dataSaver"];
         hash = responseData["chapter"]["hash"];
         baseUrl = responseData["baseUrl"];
       });
-    } catch(e) {
+    } catch (e) {
       print(e);
     }
 
     try {
       pages.length = dataSaver.length;
-      for(int i = 0; i < dataSaver.length; i++){
+      for (int i = 0; i < dataSaver.length; i++) {
         var fileName = dataSaver[i];
         var url = "$baseUrl/data-saver/$hash/$fileName";
         pages[i] = url;
       }
-    } catch(e) {
+    } catch (e) {
       print(e);
     }
   }
+
+  Future loadData() async {
+    setState(() {
+      isLoading = true;
+    });
+    await Future.wait(
+        pages.map((imageUrl) => cacheImage(context, imageUrl)).toList());
+    setState(() {
+      isLoading = false;
+    });
+  }
+
+  Future cacheImage(BuildContext context, String imageUrl) =>
+      precacheImage(CachedNetworkImageProvider(imageUrl), context);
 
   @override
   void initState() {
     super.initState();
     onLoad();
+    WidgetsBinding.instance.addPostFrameCallback((_) => loadData());
   }
 
   @override
@@ -81,13 +93,29 @@ class _MangaContentState extends State<MangaContent> {
           ],
         ),
       ),
-      body: ListView.builder(
+      body: isLoading
+          ? const Center(child: CircularProgressIndicator())
+          :ListView.builder(
           itemCount: pages.length,
           itemBuilder: (context, index) {
-        return Image.network(pages[index],
-        fit: BoxFit.fitWidth,
-        );
-      }),
+            final imageUrl = pages[index];
+            return buildImage(imageUrl);
+          }),
+    );
+  }
+
+  Widget buildImage(String imageUrl) {
+    return Center(
+      child: CachedNetworkImage(
+        imageUrl: imageUrl,
+        placeholder: (BuildContext context, String url) =>  Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [CircularProgressIndicator(color: Theme.of(context).colorScheme.tertiary,), SizedBox(height: double.maxFinite,)]),
+        ),
+        errorWidget: (BuildContext context, String url, dynamic error) => const Icon(Icons.error),
+      ),
     );
   }
 }
