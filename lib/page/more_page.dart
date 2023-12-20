e90761e703e0c2e8cb6a8c71220dd3d94c22e82e
Crunchies3 <113993166+Crunchies3@users.noreply.github.com>
2023-12-20 12:28:07 +0800

    added more page which shows details of the user, the logout button, etc.

--- lib/page/more_page.dart
+++ lib/page/more_page.dart
@@ -3,6 +3,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 import '../my_classes/services/firestore.dart';
+
 class MorePage extends StatefulWidget {
   const MorePage({super.key});
 
@@ -11,7 +12,6 @@ class MorePage extends StatefulWidget {
 }
 
 class _MorePageState extends State<MorePage> {
-
   final FireStoreService fireStoreService = FireStoreService();
 
   void signUserOut() {
@@ -23,21 +23,89 @@ class _MorePageState extends State<MorePage> {
     return Scaffold(
       body: FutureBuilder<DocumentSnapshot<Object?>>(
         future: fireStoreService.getCurrentUserDetails(),
-        builder: (context, snapshot){
-          if (snapshot.connectionState == ConnectionState.waiting){
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
             return const Center(
               child: CircularProgressIndicator(),
             );
-          }else if (snapshot.hasError) {
+          } else if (snapshot.hasError) {
             return Text("Error: ${snapshot.error}");
           } else if (snapshot.hasData) {
-            Map<String, dynamic>? user = snapshot.data!.data() as Map<String, dynamic>?;
-            return Column(
-              children: [
-                Text(user!['email']),
-                Text(user['username']),
-                Center(child: IconButton(onPressed: signUserOut, icon: Icon(Icons.logout)))
-              ],
+            Map<String, dynamic>? user =
+                snapshot.data!.data() as Map<String, dynamic>?;
+            return Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Divider(
+                    height: 20,
+                    thickness: 1,
+                    endIndent: 0,
+                    color: Theme.of(context).colorScheme.secondary,
+                  ),
+                  SizedBox(
+                    height: 25,
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                        color: Theme.of(context).colorScheme.primary,
+                        borderRadius: BorderRadius.circular(24)),
+                    padding: EdgeInsets.all(25),
+                    child: Icon(
+                      Icons.person,
+                      size: 64,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 25,
+                  ),
+                  Text(
+                    user!['username'],
+                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(user['email'],
+                    style: TextStyle(
+                      color: Colors.grey[600]
+                    ),
+                  ),
+
+                  SizedBox(
+                    height: 25,
+                  ),
+
+                  Divider(
+                    height: 20,
+                    thickness: 1,
+                    endIndent: 0,
+                    color: Theme.of(context).colorScheme.secondary,
+                  ),
+
+                  ListTile(
+                    leading: Icon(Icons.edit, color: Theme.of(context).colorScheme.tertiary),
+                    title: Text("Edit Username"),
+                    onTap: () {
+
+                    },
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.info_outline, color: Theme.of(context).colorScheme.tertiary),
+                    title: Text("About"),
+                    onTap: () {
+
+                    },
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.logout, color: Theme.of(context).colorScheme.tertiary,),
+                    title: Text("Logout"),
+                    onTap: () {
+                      signUserOut();
+                    },
+                  ),
+                ],
+              ),
             );
           } else {
             return Text("No Data");
